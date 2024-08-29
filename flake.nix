80dbac1f219fc5e8a134d2745c50bb6e4b23e6a0
haydn <hwjingram@gmail.com>
2024-08-29 09:05:33 +0100

    Dynamic hardware

--- flake.nix
+++ flake.nix
@@ -14,12 +14,22 @@
   let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    machine-id = builtins.readFile "/etc/machine-id";
+    systemHardware =
+      if machine-id == "fe889dcd8af841be9944a24ea4f635f1" then
+        ./hardware/t14.nix
+      else
+        throw "Unregistered machine ID"
+
   in
   {
     nixosConfigurations = {
       work = nixpkgs.lib.nixosSystem {
 	specialArgs = {inherit inputs;};
-	modules = [ ./hosts/work/configuration.nix ];
+	modules = [
+	  systemHardware
+	  ./hosts/work/configuration.nix
+	];
       };
     };
   };
--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -6,8 +6,7 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+    [
       inputs.home-manager.nixosModules.default
     ];
 
