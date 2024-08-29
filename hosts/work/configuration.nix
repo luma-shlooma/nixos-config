d8c4bd845ea71f2b0bfb3e18c6b31851b2c9693c
haydn <hwjingram@gmail.com>
2024-08-29 11:50:45 +0100

    Abstract modules

--- hosts/work/configuration.nix
+++ hosts/work/configuration.nix
@@ -7,7 +7,10 @@
 {
   imports =
     [
+      # Home Manager
       inputs.home-manager.nixosModules.default
+      # NixOS modules
+      ../../modules/nixos/time.nix
     ];
 
   # Bootloader.
@@ -27,24 +30,6 @@
   # Enable Bluetooth
   hardware.bluetooth.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "Europe/London";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_GB.UTF-8";
-    LC_IDENTIFICATION = "en_GB.UTF-8";
-    LC_MEASUREMENT = "en_GB.UTF-8";
-    LC_MONETARY = "en_GB.UTF-8";
-    LC_NAME = "en_GB.UTF-8";
-    LC_NUMERIC = "en_GB.UTF-8";
-    LC_PAPER = "en_GB.UTF-8";
-    LC_TELEPHONE = "en_GB.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
@@ -55,15 +40,6 @@
     # TODO: SDDM theme
   };
   services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "gb";
-    variant = "";
-  };
-
-  # Configure console keymap
-  console.keyMap = "uk";
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
