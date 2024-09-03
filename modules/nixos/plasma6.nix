bea110119eeba9468c3facb72a998bc60b5897f6
haydn <hwjingram@gmail.com>
2024-09-03 08:41:18 +0100

    Revert SDDM

--- modules/nixos/plasma6.nix
+++ modules/nixos/plasma6.nix
@@ -3,16 +3,19 @@
 {
   # Get SDDM theme
   environment.systemPackages = [
-    (pkgs.catppuccin-sddm.override {
-      flavor = "mocha";
-    })
+    # KWallet
+    pkgs.kdePackages.kwallet-pam
+    # Themes
+    #(pkgs.catppuccin-sddm.override {
+    #  flavor = "mocha";
+    #})
     #pkgs.sddm-sugar-dark
   ];
   # Enable the KDE Plasma Desktop Environment.
   services.desktopManager.plasma6.enable = true;
   services.displayManager.sddm = {
     enable = true;
-    theme = "catppuccin-mocha";
+    #theme = "catppuccin-mocha";
     #package = pkgs.kdePackages.sddm;
   };
   # Remove plasma6 packages
