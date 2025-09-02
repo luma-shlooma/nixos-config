a1aced6e1d9171f8d2011949aeb0994f85c8f584
haydn <hwjingram@gmail.com>
2025-09-02 22:22:53 +0100

    Fix and expand firefox bookmarks
    1444223b5917d9da2a1aa183181d16db07c5dd28
    
    c9e3241f1692f7687078894b14d5f119bf0cde61
    
    bfe900078c13b83c93dd8893aace57257f609358

--- modules/home-manager/firefox.nix
+++ modules/home-manager/firefox.nix
@@ -22,44 +22,119 @@
         force = true;
         settings = [
           {
-            name = "Nix Resources";
+            name = "Toolbar";
             toolbar = true;
             bookmarks = [
               {
-                name = "Packages";
-                url = "https://search.nixos.org/packages";
-                keyword = "packages";
+                name = "Nix Resources";
+                toolbar = false;
+                bookmarks = [
+                  {
+                    name = "Packages";
+                    url = "https://search.nixos.org/packages";
+                    keyword = "packages";
+                  }
+                  {
+                    name = "Home Manager";
+                    url = "https://home-manager-options.extranix.com/release=master";
+                    keyword = "home manager";
+                  }
+                  # Additional resources here
+                  # ...
+                ];
               }
               {
-                name = "Home Manager";
-                url = "https://home-manager-options.extranix.com/release=master";
-                keyword = "home manager";
+                name = "Tool Resources";
+                toolbar = false;
+                bookmarks = [
+                  {
+                    name = "Yazi";
+                    url = "https://yazi-rs.github.io/docs/quick-start/";
+                  }
+                  {
+                    name = "Vim";
+                    url = "https://vim.rtorr.com/";
+                  }
+                  {
+                    name = "Nixvim";
+                    url = "https://mattsturgeon.github.io/nixvim/";
+                  }
+                  {
+                    name = "Hyprland Wiki";
+                    url = "https://wiki.hypr.land/Configuring/";
+                  }
+                ];
               }
-            ];
-          }
-          {
-            name = "Tool Resources";
-            toolbar = true;
-            bookmarks = [
               {
-                name = "Yazi";
-                url = "https://yazi-rs.github.io/docs/quick-start/";
+                name = "Web Tools";
+                toolbar = false;
+                bookmarks = [
+                  {
+                    name = "Gmail";
+                    url = "https://mail.google.com/";
+                  }
+                  {
+                    name = "Github";
+                    url = "https://github.com/";
+                  }
+                  {
+                    name = "Nerd Fonts";
+                    url = "https://nerdfonts.com/";
+                  }
+                ];
               }
               {
-                name = "Vim";
-                url = "https://vim.rtorr.com/";
+                name = "Media";
+                toolbar = false;
+                bookmarks = [
+                  {
+                    name = "Youtube";
+                    url = "https://youtube.com/";
+                  }
+                  {
+                    name = "Twitch";
+                    url = "https://twitch.tv/";
+                  }
+                  {
+                    name = "Reddit";
+                    url = "https://reddit.com/";
+                  }
+                ];
               }
-            ];
-          }
-          {
-            name = "Media";
-            toolbar = true;
-            bookmarks = [
               {
-                name = "Youtube";
-                url = "https://youtube.com";
-                keyword = "youtube";
+                name = "Games";
+                toolbar = false;
+                bookmarks = [
+                  {
+                    name = "Itch";
+                    url = "https://itch.io/";
+                  }
+                  {
+                    name = "Wikis+";
+                    toolbar = false;
+                    bookmarks = [
+                      {
+                        name = "Deltarune";
+                        url = "https://deltarune.wiki/";
+                      }
+                      {
+                        name = "Warframe";
+                        url = "https://wiki.warframe.com/";
+                      }
+                      {
+                        name = "Warframe Market";
+                        url = "https://warframe.market/";
+                      }
+                      {
+                        name = "Vintage Story";
+                        url = "https://wiki.vintagestory.at";
+                      }
+                    ];
+                  }
+                ];
               }
+              # Additional bookmark folders here
+              # ...
             ];
           }
         ];
